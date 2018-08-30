816a66310175e8d97c7a34464bfe0a9fc42a7914
Farrukh Amir <34297140+FarrukhAmiir@users.noreply.github.com>
2018-08-30 13:08:43 +0500

    changres

---
+++ Assets/Scripts/DragandDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragandDrop : MonoBehaviour {
+
+    public GameObject DraggedObject;
+    public GameObject DroppingObject;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Drag()
+    {
+        DraggedObject.transform.position = Input.mousePosition;
+       
+    }
+
+    public void Drop()
+    {
+
+        float distance = Vector3.Distance(DraggedObject.transform.position, DroppingObject.transform.position);
+        if (distance < 70f)
+        {
+            DraggedObject.transform.position = DroppingObject.transform.position;
+        }
+    }
+}
